540241d12ce6d89a3844dca3db98a54d1f90e034
Douglas Wender <dglswender@gmail.com>
2021-07-02 17:09:42 -0300

    update

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
 
 const program = require('commander');
-const package = require('./package.json');
+const package = require('../package.json');
 
 program.version(package.version);
 
